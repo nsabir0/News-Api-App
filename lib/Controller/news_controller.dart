2b945d8fadb93b6c4122ef9b68380d6f3486c9b0
nsabir0 <nsakib472@gmail.com>
2023-10-29 18:45:24 +0600

    news app using getx.

---
+++ lib/Controller/news_controller.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+import 'package:news_api_app/Service/api_service.dart';
+
+class NewsController extends GetxController{
+
+  ApiService apiService = ApiService();
+
+  loadData()async{
+
+  }
+}
